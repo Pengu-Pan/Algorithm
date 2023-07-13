c9ffe1238e3d9814241431db724794f75bb0c00a
Pengu <18774831033@163.com>
2023-07-13 22:28:52 +0800

    链表顺序添加

--- src/main/java/org/example/dataStructure/SingleLinkedListDemo.java
+++ src/main/java/org/example/dataStructure/SingleLinkedListDemo.java
@@ -6,12 +6,20 @@ public class SingleLinkedListDemo {
         HeroNode heroNode1 = new HeroNode(1,"宋江", "及时雨");
         HeroNode heroNode2 = new HeroNode(2,"卢俊义","玉麒麟");
         HeroNode heroNode3 = new HeroNode(3,"吴用","智多星");
+        HeroNode heroNode4 = new HeroNode(4, "林冲", "豹子头");
         //创建链表
-        SingleLinkedList singleLinkedList = new SingleLinkedList();
+/*        SingleLinkedList singleLinkedList = new SingleLinkedList();
         singleLinkedList.add(heroNode1);
         singleLinkedList.add(heroNode2);
         singleLinkedList.add(heroNode3);
-        singleLinkedList.showList();
+        singleLinkedList.showList();*/
+        //顺序创建链表
+        SingleLinkedList singleLinkedList2 = new SingleLinkedList();
+        singleLinkedList2.addByOrder(heroNode1);
+        singleLinkedList2.addByOrder(heroNode4);
+        singleLinkedList2.addByOrder(heroNode2);
+        singleLinkedList2.addByOrder(heroNode3);
+        singleLinkedList2.showList();
     }
 }
 
@@ -32,6 +40,33 @@ class SingleLinkedList{
             temp = temp.next;
         }
         // 如果遍历到了末尾，则将末尾变量指向新增节点
+        temp.next = heroNode;
+    }
+
+    // 按照顺序添加节点（根据节点id）
+    public void addByOrder(HeroNode heroNode){
+        //找到要添加的节点位置,引入临时变量
+        HeroNode temp = head;
+        //判断是否已存在的标志
+        Boolean existFlag = false;
+        while(true){
+            if(temp.next == null){
+                break;
+            }
+            if(heroNode.id < temp.next.id){
+                break;
+            }
+            if (heroNode.id == temp.next.id) {
+                existFlag = true;
+                break;
+            }
+            temp = temp.next;
+        }
+        //判断是否已存在，插入
+        if(existFlag){
+            System.out.printf("准备插入的英雄的编号 %d 已经存在了, 不能加入\n", heroNode.id);
+        }
+        heroNode.next = temp.next;
         temp.next = heroNode;
     }
 
@@ -74,6 +109,6 @@ class HeroNode{
         return "HeroNode{" +
                 "id=" + id +
                 ", name='" + name + '\'' +
-                ", nickname='" + nickname + '\'';
+                ", nickname='" + nickname + '\''+"}";
     }
 }
