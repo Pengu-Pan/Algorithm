0cd7d99d61b8359894e72aedfbf9ffb144df93fa
Pengu <18774831033@163.com>
2023-07-20 21:31:50 +0800

    链表顺序添加

---
+++ src/main/java/org/example/dataStructure/Test.java
@@ -0,0 +1,10 @@
+package org.example.dataStructure;
+
+public class Test {
+    public static void main(String[] args) {
+        int length =5;
+        for (int i = length; i >=0 ; i--) {
+            System.out.println(i+"\n");
+        }
+    }
+}
